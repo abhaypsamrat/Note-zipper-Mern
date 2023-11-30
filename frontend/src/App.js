9f273ad86ac9a07d678453ac9ae98458bd6ef494
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-11-30 12:45:02 +0530

    created MyNotes page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import LandingPage from "./screen/LandingPage/LandingPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import MyNotes from "./screen/MyNotes/MyNotes";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-      <main style={{ minHeight: "93vh" }}></main>
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/mynotes" element={ <MyNotes />} />
+        </Routes>
+      </main>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <Navbar
+      expand="lg"
+      bg="primary"
+      variant="dark"
+      className="bg-body-tertiary"
+    >
+      <Container fluid>
+        <Navbar.Brand>
+          <Link to='/'>Note-Zipper</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+            </Form>
+          </Nav>
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="/mynotes">
+              <Link to="/mynotes">
+              My Notes
+              </Link>
+              </Nav.Link>
+
+            <NavDropdown title="Abhay" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Profile</NavDropdown.Item>
+
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
